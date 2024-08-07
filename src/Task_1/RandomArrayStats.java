eabec2632d257f0e560cf9aebb77fb04bf22fe81
Vyacheslav Zhukov <slava.slava.zhukov@yandex.by>
2024-08-07 15:38:09 +0300

    Добавлен код для класса RandomArrayStats

--- src/Task_1/RandomArrayStats.java
+++ src/Task_1/RandomArrayStats.java
@@ -1,2 +1,39 @@
-package Task_1;public class RandomArrayStats {
+package Task_1;
+
+import java.util.Arrays;
+
+public class RandomArrayStats {
+    public static void main(String[] args) {
+        int arraySize = 10; // Задаем размер массива
+        double[] array = new double[arraySize];
+
+        // Заполняем массив случайными числами
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = Math.random();
+        }
+
+        // Находим минимальное, максимальное и среднее значение
+        double min = array[0];
+        double max = array[0];
+        double sum = 0;
+
+        for (double num : array) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+            sum += num;
+        }
+
+        double average = sum / arraySize;
+
+        // Выводим результаты
+        System.out.println("Массив: " + Arrays.toString(array));
+        System.out.println("Минимальное значение: " + min);
+        System.out.println("Максимальное значение: " + max);
+        System.out.println("Среднее значение: " + average);
+    }
 }
+
